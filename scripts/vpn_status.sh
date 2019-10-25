02855109cc5a0a6aecd22dfbba56d0e62aa36646
Byron Kim <nocode99@gmail.com>
2019-10-25 16:33:47 -0400

    renamed

--- scripts/vpn_status.sh
+++ scripts/vpn_status.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/helpers.sh"
 
-vpn_status_on() {
+vpn_status() {
   if command_exists "nmcli"; then
     local vpn_status=$(nmcli c show --active | grep -c 'tun')
     if [ $vpn_status -eq 1 ]; then
@@ -18,7 +18,7 @@ vpn_status_on() {
 }
 
 main() {
-  vpn_status_on
+  vpn_status
 }
 
 main
