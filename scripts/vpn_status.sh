870b290772fac8f7ab309d9dde01040a2092933f
Byron Kim <nocode99@gmail.com>
2019-10-25 16:59:52 -0400

    added linux conditional

--- scripts/vpn_status.sh
+++ scripts/vpn_status.sh
@@ -5,15 +5,18 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 vpn_status() {
-  if command_exists "nmcli"; then
-    local vpn_status=$(nmcli c show --active | grep -c 'tun')
-    if [ $vpn_status -eq 1 ]; then
-      echo -e " \U1f512 "
+  if is_linux ; then
+    if command_exists "nmcli"; then
+      local vpn_status=$(nmcli c show --active | grep -c 'tun')
+      if [ $vpn_status -eq 1 ]; then
+        # \U1512 unicode for lock icon
+        echo -e " \U1f512 "
+      else
+        echo -e " "
+      fi
     else
       echo -e " "
     fi
-  else
-    echo "nmcli does not exist"
   fi
 }
 
